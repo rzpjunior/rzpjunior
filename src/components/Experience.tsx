ace666327bda779e72676886cced5454743f10eb
Junior <42242873+rzpjunior@users.noreply.github.com>
2024-09-11 23:13:30 +0700

    Update Experience.tsx

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -391,7 +391,7 @@ const Experience: React.FC = () => {
                 </li>
             </ol>
             <a 
-                href="https://1o9rejpe1ewqxu5l.public.blob.vercel-storage.com/RizkyPJunior_Resume-0RGNMTvVYBcTwN8KK0zDSLsjf1OFbI.pdf"
+                href="https://1o9rejpe1ewqxu5l.public.blob.vercel-storage.com/RizkyPJunior_Resume-iKLCGBst9ejzlOI5QxJKXNNy9WTrJD.pdf"
                 target="_blank"
                 rel="noreferrer noopener"
                 className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 font-semibold text-slate-200 group/link text-base"
